3eb62dc1420063f35f453310d1118ea037e627d7
Max Berliński <86601983+GreenSlimeStudios@users.noreply.github.com>
2025-11-28 22:33:00 +0100

    creates new samples for all 3 subdatafolders:

--- grayscale.py
+++ grayscale.py
@@ -5,41 +5,45 @@ import os
 import random
 import math
 
-images=glob.glob("./data/images/b*")
+folders=["test","train","valid"]
 
-for i in range(len(images)):
-    image = Image.open(images[i]);
-    width, height = image.size;
-    newheight=(300*height)/width;
-    new_image = image.resize((300, int(newheight)));
-    print(("./newdata/images/"+images[i][14::]))
-    new_image.save(("./newdata/images/"+images[i][14::]));
+for j in range(len(folders)):
 
-labels=glob.glob("./data/labels/b*")
-for i in range(len(labels)):
-    os.system("cp "+labels[i]+" ./newdata/labels/"+labels[i][14::])
-
-
-images=glob.glob("./newdata/images/b*")
-for i in range(len(images)):
-    if i%3==0:
-        print(images[i])
-        image_file = Image.open(images[i]) 
-        image_file = image_file.convert('LA')
-        image_file.save(("./newdata/images/grayscale-"+images[i][17:len(images[i])-3:]+".png"))
-    else:
-        image = Image.open(images[i])
+    images=glob.glob("./data/"+folders[j]+"/images/b*")
+    
+    for i in range(len(images)):
+        image = Image.open(images[i]);
         width, height = image.size;
-        new_image = image.resize((int(math.floor(random.randrange(7,15)*width/13)),int(math.floor(random.randrange(7,13)*height/10))));
-        new_image.save(("./newdata/images/squed-"+images[i][17::]));
-
-
-
-labels=glob.glob("./newdata/labels/b*")
-for i in range(len(labels)):
-    if i%3==0:
-        os.system("cp "+labels[i]+" ./newdata/labels/grayscale-"+labels[i][17::])
-    else:
-        os.system("cp "+labels[i]+" ./newdata/labels/squed-"+labels[i][17::])
+        newheight=(300*height)/width;
+        new_image = image.resize((300, int(newheight)));
+        print(("./newdata/"+folders[j]+"/images/"+images[i][14+1+len(folders[j])::]))
+        new_image.save(("./newdata/"+folders[j]+"/images/"+images[i][14+1+len(folders[j])::]));
+    
+    labels=glob.glob("./data/"+folders[j]+"/labels/b*")
+    for i in range(len(labels)):
+        os.system("cp "+labels[i]+" ./newdata/"+folders[j]+"/labels/"+labels[i][14+1+len(folders[j])::])
+    
+    
+    images=glob.glob("./newdata/"+folders[j]+"/images/b*")
+    for i in range(len(images)):
+        if i%3==0:
+            print(images[i])
+            image_file = Image.open(images[i]) 
+            image_file = image_file.convert('LA')
+            image_file.save(("./newdata/"+folders[j]+"/images/grayscale-"+images[i][17+1+len(folders[j]):len(images[i])-3:]+".png"))
+        else:
+            image = Image.open(images[i])
+            width, height = image.size;
+            new_image = image.resize((int(math.floor(random.randrange(7,15)*width/13)),int(math.floor(random.randrange(7,13)*height/10))));
+            new_image.save(("./newdata/"+folders[j]+"/images/squed-"+images[i][17+1+len(folders[j])::]));
+    
+    
+    
+    labels=glob.glob("./newdata/"+folders[j]+"/labels/b*")
+    for i in range(len(labels)):
+        if i%3==0:
+            os.system("cp "+labels[i]+" ./newdata/"+folders[j]+"/labels/grayscale-"+labels[i][17+1+len(folders[j])::])
+        else:
+            os.system("cp "+labels[i]+" ./newdata/"+folders[j]+"/labels/squed-"+labels[i][17+1+len(folders[j])::])
 
 
